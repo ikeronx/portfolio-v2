5b3ca01e5a5efcc03cecce1b7c61dcb83fc232d2
Keron Williams <ikeronw@gmail.com>
2022-08-16 11:52:08 -0400

    fixed tech stack layout and added links to each icon

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,8 +12,46 @@ const About = () => {
         </div>
         <div>
           <h3 className="about-me__subtitle">My Tech Stack</h3>
-          <div className="about-me__tech-stack-list">
-            <ul>
+          <div className="about__techstack">
+            <div>
+            <span style={{paddingLeft: "0.7rem"}}><a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics" target="_blank"><ion-icon name="logo-html5"></ion-icon></a></span>
+              
+                <p>HTML5</p>
+            </div>
+            <div>
+            <span style={{paddingLeft: "0.2rem"}}><a href="https://developer.mozilla.org/en-US/docs/Learn/CSS" target="_blank"><ion-icon name="logo-css3"></ion-icon></a></span>
+                <p>CSS3</p>
+            </div>
+            <div className="end-box">
+                 <span style={{paddingLeft: "1.4rem"}}><a href="https://developer.mozilla.org/en-US/docs/Learn/JavaScript" target="_blank"><ion-icon name="logo-javascript"></ion-icon></a></span>
+                <p>JavaScript</p>
+            </div>
+            <div className="sass-box">
+                <span style={{paddingLeft: "0.2rem"}}><a href="https://sass-lang.com/" target="_blank"><ion-icon name="logo-sass"></ion-icon></a></span>
+                <p>Sass</p>
+            </div>
+            <div>
+            <span style={{paddingLeft: "0.2rem"}}><a href="https://reactjs.org/" target="_blank"><ion-icon ion-icon name="logo-react"></ion-icon></a></span>
+                <p>React</p>
+            </div>
+            <div className="end-box">
+                <span style={{paddingLeft: "1.5rem"}}><a href="https://git-scm.com/" target="_blank"><ion-icon name="logo-github"></ion-icon></a></span>
+                <p>Git & Github</p>
+            </div>
+            <div>
+                <span style={{paddingLeft: "0.5rem"}}><a href="https://www.figma.com/" target="_blank"><ion-icon name="logo-figma"></ion-icon></a></span>
+                <p>Figma</p>
+            </div>
+            <div  className="markdown-box">
+                <span style={{paddingLeft: "1.3rem"}}><a href="https://www.markdownguide.org/" target="_blank"><ion-icon name="logo-markdown"></ion-icon></a></span>
+                <p>Markdown</p>
+            </div>
+            <div className="netlify-box end-box">
+              <span style={{paddingLeft: "0.3rem"}}><a href="https://www.netlify.com/" target="_blank"><img className="netlify-img" src="https://www.svgrepo.com/show/354110/netlify.svg" alt="img of Netlify icon" /></a></span>
+                <p>Netlify</p>
+            </div>
+
+            {/* <ul>
               <li><ion-icon name="logo-html5"></ion-icon> HTML5</li>
               <li><ion-icon name="logo-css3"></ion-icon> CSS3</li>
               <li><ion-icon name="logo-javascript"></ion-icon> JavaScript</li>
@@ -23,7 +61,7 @@ const About = () => {
               <li><ion-icon name="logo-react"></ion-icon> React</li>
               <li><ion-icon name="logo-github"></ion-icon> Git & Github</li>
               <li><ion-icon name="logo-figma"></ion-icon>  Figma</li>
-            </ul>
+            </ul> */}
           </div>
         </div>
         
