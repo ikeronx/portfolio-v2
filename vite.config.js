20828eebb28c4cc96965163483eee9bbeb15fd13
Keron Williams <ikeronw@gmail.com>
2022-08-17 21:12:42 -0400

    hero padding

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/portfolio-v2/",
   plugins: [react(), legacy({
     targets: ['defaults', 'not IE 11']
   })]
