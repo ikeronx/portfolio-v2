99af62e55db516004cdb27833496805ae5600b42
Keron Williams <ikeronw@gmail.com>
2022-11-20 20:19:56 -0500

    added link for deja brew live web site

--- src/data/cardData.js
+++ src/data/cardData.js
@@ -15,8 +15,8 @@ export const data = [
         id: 2,
         title: "Déjà Brew",
         type: "Website",
-        github: "",
-        demo: '',
+        github: "https://github.com/ikeronx/deja_brew",
+        demo: 'https://ikeronx.github.io/deja_brew/',
         description: "Déjà Brew is a contemporary cafe website which features a full tab menu, testimonial carousel , reservation functionality and a custom map.",
         image: "https://64.media.tumblr.com/6df347d8120fb7e640900021b11153e4/8b2f498d287126ce-88/s2048x3072/956c0de4474ff49cfe14351f2bb7967fc1d488a6.pnj", 
         tools: ['','','HTML5', 'CSS3', 'Javascript ES6+', 'Leaflet', 'ArcGis Enterprise', 'Fslightbox', '', ''],
@@ -30,15 +30,5 @@ export const data = [
         description: "Built, coded, and designed a fully responsive web app using React and React Hooks.",
         image: "https://64.media.tumblr.com/00b4d46cf9e125a8d0cdbd90f3ea5013/061f05122a3777cd-d0/s1280x1920/678f416ee6a8cfed92d0da0d57b39a3db02f3f60.pnj",
         tools: ['', '', '', 'Javascript ES6+', 'React', 'Hooks', 'Chakra UI'],
-    },
-    // {
-    //     id: 3,
-    //     title: "Koco..",
-    //     type: "Crud App",
-    //     github: "https://www.google.com/maps/place/Christ+the+Redeemer/@-22.951911,-43.2126759,17z/data=!3m1!4b1!4m5!3m4!1s0x997fd5984aa13f:0x9dc984d7019502de!8m2!3d-22.951916!4d-43.2104872",
-    //     demo: 'https://ikeronx.github.io/bookie_app/',
-    //     description: "Koco.. is a single-page CRUD application that allows a user to keep track of their books. The user can add, edit, delete, and save books. Each book is saved to local storage via the Web Storage API.",
-    //     image: "https://64.media.tumblr.com/55da3104989a7f491dfd897ae811a57c/799fc0719d8f4ea4-28/s1280x1920/8d85988395974fb7febc683364be05612967c7f2.pnj",
-    //     tools: ['', '',  '', 'HTML5', 'CSS3', 'Javascript ES6+', ' Web Storage API', ''],
-    // }
+    }
 ];
